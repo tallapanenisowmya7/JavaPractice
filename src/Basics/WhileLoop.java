25cc7ff8c473fe34f5ef33419732c8349f813bfc
Navyat5357 <chowdarynavyat@gmail.com>
2026-02-07 09:13:33 +0530

    While loop basics

---
+++ src/Basics/WhileLoop.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class WhileLoop {
+    public static void main (String args[]){
+        System.out.println("While loop basics");
+
+        int count = 5;
+
+        while ( count >=0 ) {
+            System.out.println("Count is: "+ count);
+            //decrement count
+            count--;
+        }
+    }
+}
